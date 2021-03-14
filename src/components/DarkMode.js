d103aaf93126316c6af39e7d66be11dadf65608b
tivaiice <tiva.wk@hotmail.com>
2021-03-14 18:26:30 +0700

    first commit.

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,6 +1,41 @@
 import React from "react";
+import "../styles/DarkMode.css"
 
 const DarkMode = () => {
+    let clickedClass = "clicked";
+    const body = document.body;
+    const lightTheme = "light";
+    const darkTheme = "dark";
+    let theme;
+
+    if (localStorage) {
+        theme = localStorage.getItem("theme");
+    }
+    if (theme === lightTheme || theme === darkTheme){
+        body.classList.add(theme);
+    } else {
+        body.classList.add(lightTheme);
+    }
+
+    const switchTheme = (e) => {
+        if (theme === darkTheme){
+            body.classList.replace(darkTheme, lightTheme);
+            e.target.class.remove(clickedClass);
+            localStorage.setItem("theme", "light"); 
+        } else {
+            body.classList.replace(lightTheme, darkTheme);
+            e.target.class.add(clickedClass);
+            localStorage.setItem("theme", "dark"); 
+        }
+    };
+
+    return (
+        <button
+            className={theme === "dark" ? clickedClass : ""}
+            id="darkMode"
+            onClick={(e) => switchTheme(e)}
+        ></button>
+    )
 
 };
 
